433927e891dcb4659a65f988b8b4d0127c46a41d
James Dumas <106568094+usmc1371nerd@users.noreply.github.com>
2024-07-25 21:03:30 -0500

    Update navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -12,11 +12,7 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState ('#')
   return (
     <nav>
-     <a href="#home"onClick={() => setActiveNav ('#home')}  className={activeNav === '#home' ? 'active' : ''}><AiFillHome/></a> 
-     <a href="#about" onClick={() => setActiveNav ('#about')} className={activeNav === '#about' ? 'active' : ''} ><FaUserCircle/></a> 
-     <a href="#education" onClick={() => setActiveNav ('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsBookFill/></a> 
-     <a href="#projects"onClick={() => setActiveNav ('#projects')} className={activeNav === '#projects' ? 'active' : ''}><FaScrewdriver/></a> 
-     <a href="#contact"onClick={() => setActiveNav ('#contacts')} className={activeNav === '#contacts' ? 'active' : ''}><MdPermContactCalendar/></a> 
+ 
     
     </nav>
   )
