bd7be938e00ff72d49787d494328b4862c8246a4
James Dumas <106568094+usmc1371nerd@users.noreply.github.com>
2024-07-25 21:02:22 -0500

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -10,25 +10,7 @@ import HeaderSocials from './HeaderSocials'
 const header = () => {
   return (
     <header>
-      <div className="container header__container" id='home'>
-        <h5>Welcome to my portfolio website</h5>
-        <h1>James Dumas</h1>
-        <h5 className='text-bright'>Full Stack Developer / Teaching Assistant </h5>
-        <h6 className='text-bright'>Bottega University</h6>
-
-        <h5 className='text-bright'>Associate Developer (Contractor) </h5>
-        <h6 className='text-bright'>NiBuild</h6>
-       
-        <Button></Button>
-
-     
-      <a href='#contact'className='scroll__down'>Scroll Down</a>
-        <HeaderSocials/>
-    
-       
-
       
-      </div>
     </header>
   )
 }
