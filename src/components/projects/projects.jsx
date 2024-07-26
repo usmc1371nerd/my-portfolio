5f72c30b2fffea06d4c89e14034b85383844d533
James Dumas <106568094+usmc1371nerd@users.noreply.github.com>
2024-07-25 21:03:56 -0500

    Update projects.jsx

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -14,26 +14,7 @@ const Projects = () => {
   
   return ( 
    <section id='projects'> 
-   <h2 className='project__title'>Code / Websites</h2>
-
-
    
-   <div className="project__container">
-  
-   <a href='https://github.com/usmc1371nerd'title="Github" ><img src={Github} className='github' alt="github-img"/> <div className="projectTitle">Github</div></a>
-  
-   <a href='https://pinelogcommunitychurch.com' title="PineLog" ><img src={Pinelog} className='pinelog' alt="pinelog-img"/><div className="projectTitle">Pinelog</div></a>
-   <a href='https://github.com/usmc1371nerd/shootingnotes.github.io' title="Github repo" ><img src={Shooter} className='shooter' alt="shooter-img"/><div className="projectTitle">Github Repo</div></a>
-   <a href='https://grizzlybearmaintenance.com/' title="Grizzly Bear" ><img src={GrizzlyBear} className='grizzlybear' alt="grizzly-bear"/><div className="projectTitle">Grizzly Bear</div></a>
-   
-   
-   {/* <a href='' title="Taggedbham" link="https://taggedbham.com/" img={Tagged} />
-   <a href='' title="Devconnect" link="https://devconnect.live/" img={Dev} />
-   <a href='' title="Pinelog Church" link="https://pinelogcommunitychurch.com/" img={Pinelog} />
-   <a href='' title="Shooting Note App" link="" img={Shooter} />
-   */}
-   
-  </div>
     </section>
  
     
