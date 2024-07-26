7c9bdffa29a23a2a36949b6bab792bd6825d39cd
James Dumas <106568094+usmc1371nerd@users.noreply.github.com>
2024-07-25 20:58:10 -0500

    Update about.jsx

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,29 +8,7 @@ import GitHubCalendar from 'react-github-calendar';
 const about = () => {
   return (
     <section id='about'>
-    <h2>About Me</h2>
-    <div className="about__container container">
-
-      <div className='about'>
-        <h2> <VscCode/> How I got into coding <VscCode/> </h2>
-        <h4>  Was simply just curiosity that grew and grew. I stumbled through trying to learn own my own and creating my own websites through a CMS. 
-          I then attended a Full Stack Development bootcamp at Bottega University where I graduated and now wanting to move into a degree program. 
-          As I'm moving through my journey I'm also starting to become intrested in the ethical hacking community, and pen testing. I love listening to 
-          to Darknet Diaries by Jack Rhysider. During my off time I'm a woodworker as well and enjoy building mostly furniture.
-          Wood working lets me approach problem solving on a whole different level. I'd say my skills are pointed heavier on the front-end web development side of full stack. </h4>
-      </div>
-
-      <div className='veteran' >
-        <h2><MdMilitaryTech/>U.S. Military / DOD<MdMilitaryTech/></h2>
-        <h4> Veteran with 12+ years of Honorable Service serving on or as Active Duty, National Guard, and a civilian contractor.</h4>
-        <img src={Awards} className='awards' alt="about-img"/>
-        
-
-      </div>
-      <label htmlFor="">GITHUB</label>
-      <GitHubCalendar username="usmc1371nerd" />
-      
-    </div>
+      <h1>PLEASE GO  https://jpsportfolio.com   </h1>
     </section>
   )
 }
