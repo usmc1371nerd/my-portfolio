2d34a49529dfecf026b21ba0fd41abe1a782da53
James Dumas <106568094+usmc1371nerd@users.noreply.github.com>
2024-07-25 21:01:18 -0500

    Update experience.jsx

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -16,27 +16,7 @@ import {TbSql} from 'react-icons/tb'
 const experience = () => {
   return (
     <section id='education'> 
-    <h2>Education</h2>
-  
-    <div className="education">
-    
-    <h3 className='title'> Full Stack Development Certificate </h3>
-    <h4 className='title-education'>Technologies studied:
-    <div className="list">
-    <ul>HTML <SiHtml5/> </ul>
-    <ul>CSS <DiCss3/></ul>
-    <ul>JavaScript<TbBrandJavascript/> </ul>
-    <ul>Python <SiPython/></ul>
-    <ul>React <FaReact/></ul>
-    <ul>Flask <SiFlask/></ul>
-    <ul>MongoDB <SiMongodb/></ul>
-    <ul>SQL Databases <TbSql/></ul>
-    </div>
-    </h4>
-    <img src={Bottega} className='bottega' alt="education-img"/>
-        
-    </div>
-  
+   
     
     
     </section>
